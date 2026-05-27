80225636be420742fd2de101a6b0375f0cb0ee8e
MrTimnn <pv23009@alunos.estgv.ipv.pt>
2026-05-27 19:29:54 +0100

    correção emulador

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import '../services/session.dart';
 
 class ApiService {
-  static const String baseUrl = "http://192.168.1.225:3000";
+  static const String baseUrl = "http://10.0.2.2:3000";
 
   static bool _isJson(String body) {
     final text = body.trim();
